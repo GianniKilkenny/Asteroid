29bdb0e9a7b6389d5e131379bcd490aee71fce6d
GianniKilkenny <giannikilkenny22404@gmail.com>
2026-04-13 12:25:49 -0700

    Added player class, and drew player in middle of screen

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import pygame
 from constants import *
+from player import *
 from logger import log_state
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player1 = Player(x = SCREEN_WIDTH / 2,y = SCREEN_HEIGHT / 2)
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type ==pygame.QUIT:
                 return
         screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
-
-    print("Starting Asteroids with pygame version: VERSION")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
